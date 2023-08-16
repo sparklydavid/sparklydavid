4067279450c01be032828244930dbdb97bfe2f79
Sparkly David <87685038+sparklydavid@users.noreply.github.com>
2023-08-16 16:07:41 +0800

    big update

--- js/navbar.js
+++ js/navbar.js
@@ -1,38 +1,26 @@
 var hamburgerStatus = false;
 
 function hamburgerClicked() {
-    var menuVerticle = document.getElementById('m-menu');
+    var subMenu = document.getElementById('sub-menu');
     var hamburgerImg = document.getElementById('hamburger-img');
+    var hamburgerTxt = document.getElementById('hamburger-txt');
     var pageCurtain = document.getElementById('page-curtain');
 
     if (hamburgerStatus == false) {
-        menuVerticle.style.display = 'block';
+        subMenu.style.display = 'block';
         hamburgerImg.src = 'img/hamburger-x.png';
+        hamburgerTxt.textContent="CLOSE";
         pageCurtain.style.display = 'block';
+
         hamburgerStatus = true;
     }
     else {
-        menuVerticle.style.display = 'none';
+        subMenu.style.display = 'none';
         hamburgerImg.src = 'img/hamburger-full.png';
+        hamburgerTxt.textContent="MENU";
         pageCurtain.style.display = 'none';
+
         hamburgerStatus = false;
     }
 }
 
-
-
-//change topbar color when scroll
-const colorChangingDiv = document.querySelector('#top-bar');
-
-window.addEventListener('scroll', () => {
-    // Calculate how far down the page the user has scrolled
-    const scrollY = window.scrollY;
-
-    // Change the background color based on the scroll position
-    // Here, we're using a threshold of 200 pixels from the top of the page
-    if (scrollY > 1) {
-        colorChangingDiv.style.backgroundColor = 'rgba(255, 255, 255, 0.9)';
-    } else {
-        colorChangingDiv.style.backgroundColor = 'transparent';
-    }
-});
