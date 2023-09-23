e1862507e4a64ee053d5cb55e8e9b7c919c3f098
Sparkly David <87685038+sparklydavid@users.noreply.github.com>
2023-09-23 19:49:54 +0800

    add subpages

---
+++ post-pages/0_js/navbar.js
@@ -0,0 +1,26 @@
+var hamburgerStatus = false;
+
+function hamburgerClicked() {
+    var subMenu = document.getElementById('sub-menu');
+    var hamburgerImg = document.getElementById('hamburger-img');
+    var hamburgerTxt = document.getElementById('hamburger-txt');
+    var pageCurtain = document.getElementById('page-curtain');
+
+    if (hamburgerStatus == false) {
+        subMenu.style.display = 'block';
+        hamburgerImg.src = 'img/hamburger-x.png';
+        hamburgerTxt.textContent="CLOSE";
+        pageCurtain.style.display = 'block';
+
+        hamburgerStatus = true;
+    }
+    else {
+        subMenu.style.display = 'none';
+        hamburgerImg.src = 'img/hamburger-full.png';
+        hamburgerTxt.textContent="MENU";
+        pageCurtain.style.display = 'none';
+
+        hamburgerStatus = false;
+    }
+}
+
