0b34fe905ba27b86d80276129666e7fd2f2518ed
Sparkly David <87685038+sparklydavid@users.noreply.github.com>
2025-01-13 00:21:57 +0800

    update

--- js/navbar_post.js
+++ js/navbar_post.js
@@ -8,7 +8,7 @@ function hamburgerClicked() {
 
     if (hamburgerStatus == false) {
         subMenu.style.display = 'block';
-        hamburgerImg.src = 'img/hamburger-x.png';
+        hamburgerImg.src = '../img/hamburger-x.png';
         hamburgerTxt.textContent="CLOSE";
         pageCurtain.style.display = 'block';
 
@@ -16,7 +16,7 @@ function hamburgerClicked() {
     }
     else {
         subMenu.style.display = 'none';
-        hamburgerImg.src = 'img/hamburger-full.png';
+        hamburgerImg.src = '../img/hamburger-full.png';
         hamburgerTxt.textContent="MENU";
         pageCurtain.style.display = 'none';
 
